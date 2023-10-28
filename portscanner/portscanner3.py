788e6080e8552f1c63380526a6533c0f6177a203
Ahortu901 <aricktech.edu@gmail.com>
2023-10-28 15:01:09 +0100

    update

---
+++ portscanner/portscanner3.py
@@ -0,0 +1,24 @@
+import socket
+from IPy import IP
+
+def check_ip(ip):
+    try:
+        IP(ip)
+        return(ip)
+    except ValueError:
+        return socket.gethostbyname(ip)
+
+def scan_port(ipaddress, port):
+    try:
+        sock.settimeout(0.5)
+        sock = socket.socket()
+        sock.connect((ipaddress, port))
+        print("[+] Port " + str(port)  + " is Open...")
+    except:
+        print("[-] Port " + str(port) + " is closed..")
+
+ipaddress = input("[+] Enter Target To Scan: ")
+converted_ip = check_ip(ipaddress)
+
+for port in range (75,85):
+    scan_port(converted_ip, port)
