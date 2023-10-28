974424b9af0ba7ce33b9de7056be387b2d12927d
Ahortu901 <aricktech.edu@gmail.com>
2023-10-28 16:04:34 +0100

    updating portscanner5

--- portscanner/portscanner5.py
+++ portscanner/portscanner5.py
@@ -1,10 +1,10 @@
 import socket
 from IPy import IP
 
-def scan(target):
+def scan(target, port_num):
     converted_ip = check_ip(target)
     print('\n' + '[-_0 Scanning Target] ' + str(target))
-    for port in range (1,100):
+    for port in range (1, int(port_num)):
         scan_port(converted_ip, port)
 
 def check_ip(ip):
@@ -31,8 +31,9 @@ def scan_port(ipaddress, port):
         pass
 
 targets = input("[+] Enter Target/s To Scan(split multiple targets with ,): ")
+port_num = input("[+] Enter the number of ports to scan: ")
 if ',' in targets:
     for ip_add in targets.split(','):
-        scan(ip_add.strip(' '))
+        scan(ip_add.strip(' '), port_num)
 else:
-    scan(targets)
+    scan(targets, port_num)
